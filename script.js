7f991d70a4fee1781f8595cd246c1e624724e47e
Eli Wood <mreliwood1@gmail.com>
2023-01-24 10:45:24 -0800

    first commit, created basic file structure

---
+++ script.js
@@ -0,0 +1,15 @@
+// import components
+const button = document.getElementById('button');
+const output = document.getElementById('output');
+
+function challenge() {
+
+    
+
+};
+
+
+// on button click, run code and display result on the page
+button.addEventListener('click', () => {
+    output.innerText = challenge();
+});
